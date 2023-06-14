b6ce6dc21b6cc4021476db503676a8c088359be1
Dicky <Dicky.Qi@icloud.com>
2023-06-14 16:21:53 +0800

    add Dilation/Erision ImMat node and update imgui

---
+++ plugin/nodes/filters/Dilation/ImMatDilationNode.cpp
@@ -0,0 +1,146 @@
+#include <UI.h>
+#include <imgui_json.h>
+#include <imgui_extra_widget.h>
+#include <ImVulkanShader.h>
+#include <Dilation_vulkan.h>
+
+#define NODE_VERSION    0x01000000
+
+namespace BluePrint
+{
+struct DilationNode final : Node
+{
+    BP_NODE_WITH_NAME(DilationNode, "Dilation", "CodeWin", NODE_VERSION, VERSION_BLUEPRINT_API, NodeType::External, NodeStyle::Default, "Filter#Video#Morph")
+    DilationNode(BP* blueprint): Node(blueprint) { m_Name = "Dilation"; }
+
+    ~DilationNode()
+    {
+        if (m_filter) { delete m_filter; m_filter = nullptr; }
+    }
+
+    void Reset(Context& context) override
+    {
+        Node::Reset(context);
+        m_mutex.lock();
+        m_MatOut.SetValue(ImGui::ImMat());
+        m_mutex.unlock();
+    }
+
+    FlowPin Execute(Context& context, FlowPin& entryPoint, bool threading = false) override
+    {
+        auto mat_in = context.GetPinValue<ImGui::ImMat>(m_MatIn);
+        if (!mat_in.empty())
+        {
+            int gpu = mat_in.device == IM_DD_VULKAN ? mat_in.device_number : ImGui::get_default_gpu_index();
+            if (!m_Enabled)
+            {
+                m_MatOut.SetValue(mat_in);
+                return m_Exit;
+            }
+            if (!m_filter || gpu != m_device)
+            {
+                if (m_filter) { delete m_filter; m_filter = nullptr; }
+                m_filter = new ImGui::Dilation_vulkan(gpu);
+            }
+            if (!m_filter)
+            {
+                return {};
+            }
+            m_device = gpu;
+            ImGui::VkMat im_RGB; im_RGB.type = m_mat_data_type == IM_DT_UNDEFINED ? mat_in.type : m_mat_data_type;
+            m_NodeTimeMs = m_filter->filter(mat_in, im_RGB, m_ksz);
+            m_MatOut.SetValue(im_RGB);
+        }
+        return m_Exit;
+    }
+
+    void DrawSettingLayout(ImGuiContext * ctx) override
+    {
+        // Draw Setting
+        Node::DrawSettingLayout(ctx);
+        ImGui::Separator();
+        ImGui::TextUnformatted("Mat Type:"); ImGui::SameLine();
+        ImGui::RadioButton("AsInput", (int *)&m_mat_data_type, (int)IM_DT_UNDEFINED); ImGui::SameLine();
+        ImGui::RadioButton("Int8", (int *)&m_mat_data_type, (int)IM_DT_INT8); ImGui::SameLine();
+        ImGui::RadioButton("Int16", (int *)&m_mat_data_type, (int)IM_DT_INT16); ImGui::SameLine();
+        ImGui::RadioButton("Float16", (int *)&m_mat_data_type, (int)IM_DT_FLOAT16); ImGui::SameLine();
+        ImGui::RadioButton("Float32", (int *)&m_mat_data_type, (int)IM_DT_FLOAT32);
+    }
+
+    bool CustomLayout() const override { return true; }
+    bool Skippable() const override { return true; }
+
+    bool DrawCustomLayout(ImGuiContext * ctx, float zoom, ImVec2 origin, ImGui::ImCurveEdit::keys * key) override
+    {
+        ImGui::SetCurrentContext(ctx);
+        ImGuiSliderFlags flags = ImGuiSliderFlags_AlwaysClamp; // ImGuiSliderFlags_NoInput
+        bool changed = false;
+        int _ksz = m_ksz;
+        ImGui::Dummy(ImVec2(200, 8));
+        ImGui::PushItemWidth(200);
+        ImGui::BeginDisabled(!m_Enabled);
+        ImGui::SliderInt("Kernel Size##Dilation", &_ksz, 1, 30, "%d", flags);
+        ImGui::SameLine(320);  if (ImGui::Button(ICON_RESET "##reset_size##Dilation")) { _ksz = 3; changed = true; }
+        ImGui::PopItemWidth();
+        if (_ksz != m_ksz) { m_ksz = _ksz; changed = true; }
+        ImGui::EndDisabled();
+        return changed;
+    }
+
+    int Load(const imgui_json::value& value) override
+    {
+        int ret = BP_ERR_NONE;
+        if ((ret = Node::Load(value)) != BP_ERR_NONE)
+            return ret;
+
+        if (value.contains("mat_type"))
+        {
+            auto& val = value["mat_type"];
+            if (val.is_number()) 
+                m_mat_data_type = (ImDataType)val.get<imgui_json::number>();
+        }
+        if (value.contains("ksize"))
+        {
+            auto& val = value["ksize"];
+            if (val.is_number()) 
+                m_ksz = val.get<imgui_json::number>();
+        }
+        return ret;
+    }
+
+    void Save(imgui_json::value& value, std::map<ID_TYPE, ID_TYPE> MapID) override
+    {
+        Node::Save(value, MapID);
+        value["mat_type"] = imgui_json::number(m_mat_data_type);
+        value["ksize"] = imgui_json::number(m_ksz);
+    }
+
+    void DrawNodeLogo(ImGuiContext * ctx, ImVec2 size, std::string logo) const override
+    {
+        Node::DrawNodeLogo(ctx, size, std::string(u8"\uea37"));
+    }
+
+    span<Pin*> GetInputPins() override { return m_InputPins; }
+    span<Pin*> GetOutputPins() override { return m_OutputPins; }
+    Pin* GetAutoLinkInputFlowPin() override { return &m_Enter; }
+    Pin* GetAutoLinkOutputFlowPin() override { return &m_Exit; }
+    vector<Pin*> GetAutoLinkInputDataPin() override { return {&m_MatIn}; }
+    vector<Pin*> GetAutoLinkOutputDataPin() override { return {&m_MatOut}; }
+
+    FlowPin   m_Enter   = { this, "Enter" };
+    FlowPin   m_Exit    = { this, "Exit" };
+    MatPin    m_MatIn   = { this, "In" };
+    MatPin    m_MatOut  = { this, "Out" };
+
+    Pin* m_InputPins[2] = { &m_Enter, &m_MatIn };
+    Pin* m_OutputPins[2] = { &m_Exit, &m_MatOut };
+
+private:
+    ImDataType m_mat_data_type {IM_DT_UNDEFINED};
+    int m_device            {-1};
+    ImGui::Dilation_vulkan * m_filter {nullptr};
+    int m_ksz {3};
+};
+} //namespace BluePrint
+
+BP_NODE_DYNAMIC_WITH_NAME(DilationNode, "Dilation", "CodeWin", NODE_VERSION, VERSION_BLUEPRINT_API, BluePrint::NodeType::External, BluePrint::NodeStyle::Default, "Filter#Video#Morph")
