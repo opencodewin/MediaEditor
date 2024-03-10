b8e93485e89a074bcb65fc5d7fd47f1406a6bef8
Dicky <Dicky.Qi@icloud.com>
2024-03-11 00:17:21 +0800

    1. add RGB2YUV node setting 2. update submodules

--- plugin/nodes/media/MatRGB2YUV/ImMatRGB2YUVNode.cpp
+++ plugin/nodes/media/MatRGB2YUV/ImMatRGB2YUVNode.cpp
@@ -136,6 +136,8 @@ struct MatRGBA2YUVNode final : Node
         // Draw Setting
         auto changed = Node::DrawSettingLayout(ctx);
         int color_format = m_color_format;
+        int color_space = m_color_space;
+        int color_range = m_color_range;
         ImGui::Separator();
         changed |= Node::DrawDataTypeSetting("Mat Type:", m_mat_data_type);
         ImGui::Separator();
@@ -144,11 +146,17 @@ struct MatRGBA2YUVNode final : Node
         ImGui::RadioButton("YUV440",  (int *)&color_format, IM_CF_YUV440);
         ImGui::RadioButton("YUV444",   (int *)&color_format, IM_CF_YUV444);
         ImGui::RadioButton("NV12",   (int *)&color_format, IM_CF_NV12);
-        if (m_color_format != color_format)
-        {
-            m_color_format = (ImColorFormat)color_format;
-            changed = true;
-        }
+        ImGui::Separator();
+        ImGui::RadioButton("BT601",  (int *)&color_space, IM_CS_BT601);
+        ImGui::RadioButton("BT709",   (int *)&color_space, IM_CS_BT709);
+        ImGui::RadioButton("BT2020",  (int *)&color_space, IM_CS_BT2020);
+        ImGui::Separator();
+        ImGui::RadioButton("Full Range",  (int *)&color_range, IM_CR_FULL_RANGE);
+        ImGui::RadioButton("Narrow Range",   (int *)&color_range, IM_CR_NARROW_RANGE);
+
+        if (m_color_format != color_format) { m_color_format = (ImColorFormat)color_format; changed = true; }
+        if (m_color_space != color_space) { m_color_space = (ImColorSpace)color_space; changed = true; }
+        if (m_color_range != color_range) { m_color_range = (ImColorRange)color_range; changed = true; }
         return changed;
     }
 
@@ -163,12 +171,33 @@ struct MatRGBA2YUVNode final : Node
             if (val.is_number()) 
                 m_mat_data_type = (ImDataType)val.get<imgui_json::number>();
         }
+        if (value.contains("color_format"))
+        {
+            auto& val = value["color_format"];
+            if (val.is_number()) 
+                m_color_format = (ImColorFormat)val.get<imgui_json::number>();
+        }
+        if (value.contains("color_space"))
+        {
+            auto& val = value["color_space"];
+            if (val.is_number()) 
+                m_color_space = (ImColorSpace)val.get<imgui_json::number>();
+        }
+        if (value.contains("color_range"))
+        {
+            auto& val = value["color_range"];
+            if (val.is_number()) 
+                m_color_range = (ImColorRange)val.get<imgui_json::number>();
+        }
         return ret;
     }
     void Save(imgui_json::value& value, std::map<ID_TYPE, ID_TYPE> MapID) override
     {
         Node::Save(value, MapID);
         value["mat_type"] = imgui_json::number(m_mat_data_type);
+        value["color_format"] = imgui_json::number(m_color_format);
+        value["color_space"] = imgui_json::number(m_color_space);
+        value["color_range"] = imgui_json::number(m_color_range);
     }
 
     span<Pin*> GetInputPins() override { return m_InputPins; }
