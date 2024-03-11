486a6d804a9b584b095abab9962a7d32c655a4e0
Dicky <Dicky.Qi@icloud.com>
2024-03-12 02:48:07 +0800

    using fp16 for SmudgeBlur

--- plugin/nodes/filters/SmudgeBlur/SmudgeBlur_shader.h
+++ plugin/nodes/filters/SmudgeBlur/SmudgeBlur_shader.h
@@ -26,21 +26,20 @@ layout (push_constant) uniform parameter \n\
 " \n\
 void main() \n\
 { \n\
-    vec3 color = vec3(0, 0, 0); \n\
+    sfpvec3 color = sfpvec3(0.f); \n\
     int gx = int(gl_GlobalInvocationID.x); \n\
     int gy = int(gl_GlobalInvocationID.y); \n\
     if (gx >= p.out_w || gy >= p.out_h) \n\
         return; \n\
     vec2 uv = vec2(float(gl_GlobalInvocationID.x) / float(p.out_w - 1), float(gl_GlobalInvocationID.y) / float(p.out_h - 1)); \n\
-    for (float i = 0.0; i < 360.0 / p.iterations; i++) \n\
+    for (sfp i = sfp(0.f); i < sfp(360.f / p.iterations); i++) \n\
     { \n\
 	    vec2 uv_new = vec2((uv.x + sin(i * p.iterations) * p.radius * color.r), (uv.y + cos(i * p.iterations) * p.radius * (color.g + color.b))); \n\
         sfpvec4 color_new = load_rgba(int(uv_new.x * (p.w - 1)), int(uv_new.y * (p.h - 1)), p.w, p.h, p.cstep, p.in_format, p.in_type); \n\
-        color = color + vec3(color_new.r, color_new.g, color_new.b); \n\
+        color = color + color_new.rgb; \n\
     } \n\
-    color = color / (360.0 / p.iterations); \n\
-    sfpvec4 rgba = sfpvec4(sfp(color.x), sfp(color.y), sfp(color.z), sfp(1.0)); \n\
-    store_rgba(rgba, gx, gy, p.out_w, p.out_h, p.out_cstep, p.out_format, p.out_type); \n\
+    color = color / sfp(360.0 / p.iterations); \n\
+    store_rgba(sfpvec4(color, sfp(1.f)), gx, gy, p.out_w, p.out_h, p.out_cstep, p.out_format, p.out_type); \n\
 } \n\
 "
 
